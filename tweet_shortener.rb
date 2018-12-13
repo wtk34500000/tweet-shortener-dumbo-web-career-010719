97b092c651f026598d7a78ca218a173c9ae724ae
Weijun Huang <wjhuang0726@gmail.com>
2018-12-13 00:44:05 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def dictionary
+  words-to-be-substituted={
+    "hello": "hi",
+  "to, two, too" : '2',
+  "for, four" : '4',
+  'be' : 'b',
+  'you' : 'u',
+  "at" : "@", 
+  "and" : "&"
+  }
+end
+
+def word_substituter(tweets)
+    
+end
